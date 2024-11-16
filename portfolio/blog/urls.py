cdef6a7d3d7c12f12c13d5a30d21ecb42b6a903e
RRafi89 <rafiquzzamanrafi100@.com>
2024-11-16 19:02:13 +0600

    Completed the CRUD operation

--- portfolio/blog/urls.py
+++ portfolio/blog/urls.py
@@ -1,8 +1,20 @@
 from django.urls import path
-from .views import HomePageView, DetailPageView
+from .views import (
+    HomePageView,
+    DetailPageView,
+    BlogCreateView,
+    BlogUpdateView,
+    BlogDeleteView,
+)
 
 urlpatterns = [
-    path("", HomePageView.as_view(), name='home'),
-    path("post/<int:pk>/", DetailPageView.as_view(), name='details')
-
+    path("", HomePageView.as_view(), name="home"),
+    path("post/<int:pk>/", DetailPageView.as_view(), name="details"),
+    path("post/new/", BlogCreateView.as_view(), name="create"),
+    path("post/<int:pk>/edit/", BlogUpdateView.as_view(), name="update"),
+    path(
+        "post/<int:pk>/delete/",
+        BlogDeleteView.as_view(),
+        name="delete",
+    ),
 ]
--- portfolio/blog/views.py
+++ portfolio/blog/views.py
@@ -1,10 +1,32 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic.edit import DeleteView
 from .models import Post
+from django.urls import reverse_lazy
+
 
 class HomePageView(ListView):
     model = Post
-    template_name = 'home.html'
+    template_name = "home.html"
+
 
 class DetailPageView(DetailView):
     model = Post
-    template_name = 'detail.html'
+    template_name = "detail.html"
+
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = "create.html"
+    fields = ["title", "author", "body"]
+
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = "update.html"
+    fields = ["title", "body"]
+
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = "delete.html"
+    success_url = reverse_lazy("home")
